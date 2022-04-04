f4b105cec0e4269bcd95d94238600b5d74eb612a
Nick Janes <nickjanes@Nicks-MBP-2.home>
2022-04-04 12:27:13 -0400

    Changed the import path method

--- internal_igeekuplay_notion/app.py
+++ internal_igeekuplay_notion/app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from users import user_blueprint
+from internal_igeekuplay_notion.users import user_blueprint
 from contacts import contact_blueprint
 from events import event_blueprint
 from companies import company_blueprint
@@ -15,3 +15,4 @@ app.register_blueprint(company_blueprint, url_prefix='/companies')
 app.register_blueprint(task_blueprint, url_prefix='/tasks')
 app.register_blueprint(project_blueprint, url_prefix='/projects')
 app.register_blueprint(invoice_blueprint, url_prefix='/invoices')
+
